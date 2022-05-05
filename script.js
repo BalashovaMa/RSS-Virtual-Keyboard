786bba8edcc582f1d79692e8638239e868c1c6c1
BalashovaMa <balashova.m.nd@gmail.com>
2022-05-05 15:55:38 +0300

    feat: add style.css and index.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const outputArea = document.createElement('textarea');
+outputArea.classList.add('output-area');
+document.body.append(outputArea);
+
+const openButton = document.createElement('button');
+openButton.classList.add('open-button');
+document.body.append(openButton);
+openButton.textContent='Open keyboard';
+
